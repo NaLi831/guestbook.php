1d4052972d80f43b499a64b0387eb768f3830f30
NaLi <lkqw0669@gmail.com>
2025-10-10 15:32:23 +0300

    добавлена функция выхода из системы

---
+++ logout.php
@@ -0,0 +1,18 @@
+<?php
+require_once 'config.php';
+
+session_start();
+
+if ($_SERVER['REQUEST_METHOD'] !== 'POST') {
+    header('Location: index.php');
+    exit();
+}
+
+session_destroy();
+
+$_SESSION['flash_success'] = 'Вы вышли';
+
+header('Location: index.php');
+exit();
+
+?>

f948330257af926e4d0861681e68c3856c66129c
NaLi <lkqw0669@gmail.com>
2025-10-15 13:42:03 +0300

    добавлен функционал отправки сообщения

--- add.php
+++ add.php
@@ -1,21 +1,17 @@
 <?php
 
 require_once('config.php');
-// var_dump($_POST);
+requireLogin();
 
 if ($_POST) {
-    $name = trim($_POST['name']);
-    $message = trim($_POST['message']);
+    $message = trim($_POST['message'] ?? '');
 
-    if ($name && $message) {
-        $stmt = $pdo->prepare("INSERT INTO messages (name, message) VALUES (?, ?)");
-        $stmt->execute([$name, $message]);
-
-        header('Location: index.php');
-        exit;
-    } else {
-        $error = "Пожалуйста, заполните все поля.";
+    if ($message) {
+        $stmt = $pdo->prepare("INSERT INTO messages (user_id, message) VALUES (?, ?)");
+        $stmt->execute([getCurrentUserId(), $message]);
     }
-}
 
+    header('Location: index.php');
+    exit;
+}
 ?>
